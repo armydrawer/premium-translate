1afd13286a861b2443915d02431c558866505f82
Aleksandr Kislitsyn <55327816+armydrawer@users.noreply.github.com>
2025-07-08 14:23:11 +0300

    Delete en/.gitbook/assets directory

--- en/.gitbook/assets/createuser.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once('wp-load.php');
-$user_login = 'ЛОГИН'; // Укажите логин  нового пользователя. Логин должен быть уникальным
-$pass = 'ПАРОЛЬ'; // Укажите пароль для нового пользователя. Используйте сложный пароль
-$email = 'EMAIL'; // Укажите email для нового пользователя. Email должен быть уникальным
-wp_insert_user( array ('user_login' => $user_login, 'user_email' => $email, 'user_pass' => $pass, 'role' => 'administrator') ) ;
-?>
